fd607cb7ea1d8e82e89701cd4e080043aaade925
marius.malazinskas@acosta.com <marius.kvasnauskas@gmail.com>
2019-08-13 13:49:59 +0300

    Style, updated file locations

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots';
+import './App.css'
 
 
 const App = () => {
-    const [stateRobots] = useState(robots);
+    //StateHook
+    const [robots, setRobots] = useState([]);
     const [searchfield, setField] = useState('');
    
+    //Replaces componentDidMount()
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users =>{setRobots(users)});         
+    }, []);
+
+
+
+    //Function
     const onSearchChange = (event) => {
         setField(event.target.value);
         
         console.log(filteredRobots);
     }
-    const filteredRobots = stateRobots.filter(robot =>{
+    //Render
+    const filteredRobots = robots.filter(robot =>{
         return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
     return (
         <div className='tc'>
-            <h1>RoboFriends</h1>
+            <h1 className='f1'>RoboFriends</h1>
             <SearchBox searchChange={onSearchChange} />
             <CardList robots={filteredRobots}/>
         </div>
